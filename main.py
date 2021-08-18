1f21bf005f483851290527d671e3553653b28743
MxDkl <max@dekelnet.com>
2021-08-18 10:21:41 +0300

    Update main.py
    updating main with test

--- main.py
+++ main.py
@@ -1,44 +1,75 @@
 import asyncio
 import os
+import re
 import asyncpraw
-from asyncpraw.models import Comment
+from asyncpraw.models import Submission
 import chess
 import chess.engine
 
 
-COMMENT = """Wanna play a game?\n
-Make your move using algebraic notation.\n
-The last move was made by: {user}.\n
-This is the current position (White to play):\n
-{ascii}
-[chess.com](https://chess.com/analysis?fen={fen}) [lichess.org](https://lichess.org/analysis/standard/{fen})"""
+COMMENT = """Wanna play a game?
+Make your move using algebraic notation.
+---
+The last move was made by: {user}.
+This is the current position:
+>**White to play**: [chess.com](https://chess.com/analysis?fen={fen}) \
+[lichess.org](https://lichess.org/analysis/standard/{fen})
+**Light mode:**
+{unicode}
+**Dark mode:**
+{unicode_ic}"""
 
+def format_board_unicode(unicode: str) -> str:
+	# Split into ranks
+	unicode = unicode.splitlines()
+	# Split ranks into files
+	unicode = [r.split(" ") for r in unicode]
+	# Replace "⭘"s with empty strings
 
-async def play(comment: Comment, board: chess.Board) -> None:
-	reddit = asyncpraw.Reddit(
-		client_id="",
-		client_secret="",
-		password="",
-		user_agent="",
-		username=""
-	)
-	anarchychess = await reddit.subreddit('anarchychess')
+	for r in range(8):
+		for f in range(8):
+			if unicode[r][f] == "⭘":
+				unicode[r][f] = ""
+		unicode[r].append(str(8 - r))
+	unicode.insert(0, "||||||||".split("|"), )
+	unicode.insert(1, ":-|:-|:-|:-|:-|:-|:-|:-|:-".split("|"))
+	unicode.append("a|b|c|d|e|f|g|h|".split("|"))
 
+	return "\n".join(["|" + "|".join(r) + "|" for r in unicode])
+
+async def play(submission: Submission, board: chess.Board) -> None:
 	transport, stockfish = await chess.engine.popen_uci("/home/max/chessbot/Stockfish/src/stockfish")
 	
-	comment_id = comment.id
-	async for reply in anarchychess.stream.comments(skip_existing=True):
-		print(reply.body)
-		if reply.parent_id == comment_id:
-			try:
-				board.push_san(comment.body)
-			except:
-				continue
-			move = await stockfish.play(board, chess.engine.Limit(time=0.1))
-			board.push(move)
-			commend_md = COMMENT.format(ascii=str(board), fen=board.fen(), user=reply.author)
-			await comment.edit(commend_md)
-			print("Edited")
+	last_comment = submission.created_utc
+	while not board.is_game_over():
+		await submission.load()
+		for comment in await submission.comments():
+			if comment.created_utc > last_comment:
+				last_comment = comment.created_utc
+				print(comment.body)
+
+				try:
+					board.push_san(comment.body)
+				except ValueError:
+					print(f"Illegal move: {comment.body} at {board.fen()}")
+					continue
+
+				result = await stockfish.play(board, chess.engine.Limit(depth=18))
+				board.push(result.move)
+
+				comment_md = COMMENT.format(
+					unicode=format_board_unicode(board.unicode()), 
+					unicode_ic=format_board_unicode(board.unicode(invert_color=True)),
+					turn=chess.COLOR_NAMES[board.turn].capitalize(),
+					user=comment.author,
+					fen=board.fen()
+				)
+				await submission.edit(comment_md)
+				print("Edited")
+		
+		print("i")
+		await asyncio.sleep(3*60)
+	print("Game over")
 
 async def main() -> None:
 	reddit = asyncpraw.Reddit(
@@ -48,15 +79,24 @@ async def main() -> None:
 		user_agent="",
 		username=""
 	)
+	reddit.validate_on_submit = True
+
 	anarchychess = await reddit.subreddit('anarchychess')
+
+
 	#async for submission in anarchychess.stream.submissions(skip_existing=True):
-	for x in range(1):
-		submission = await reddit.submission(id="p5g2wb")
-		board = chess.Board("rnbq1bnr/ppppkppp/8/4p3/4P3/8/PPPPKPPP/RNBQ1BNR w - - 2 3")
-		comment_md = COMMENT.format(ascii=" ", fen=board.fen(), user="Nobody")
-		comment = await submission.reply(comment_md)
-		print("posted")
-		await play(comment, board)
+	board = chess.Board()
+	
+	comment_md = COMMENT.format(
+		unicode=format_board_unicode(board.unicode()), 
+		unicode_ic=format_board_unicode(board.unicode(invert_color=True)),
+		turn=chess.COLOR_NAMES[board.turn].capitalize(), 
+		user="Nobody",
+		fen=board.fen()
+	)
+	submission = await anarchychess.submit("I'm a bot that plays chess. Comment yout move", comment_md)
+	print("posted")
+	await play(submission, board)
 
 if __name__ == "__main__":
 	asyncio.run(main())
